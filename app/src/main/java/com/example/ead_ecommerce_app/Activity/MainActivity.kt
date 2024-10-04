b527207476898857c1285a9dcc03dd847dc669de
Binod <it21211164@my.sliit.lk>
2024-10-04 20:05:58 +0530

    Do some changes in main page

--- app/src/main/java/com/example/ead_ecommerce_app/Activity/MainActivity.kt
+++ app/src/main/java/com/example/ead_ecommerce_app/Activity/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.ead_ecommerce_app
+package com.example.ead_ecommerce_app.Activity
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.ead_ecommerce_app.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/ead_ecommerce_app/Adapter/SliderAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.ead_ecommerce_app.Adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class SliderAdapter:RecyclerView.Adapter<SliderAdapter.SliderViewholder>() {
+    class SliderViewholder {
+
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): SliderAdapter.SliderViewholder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: SliderAdapter.SliderViewholder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
