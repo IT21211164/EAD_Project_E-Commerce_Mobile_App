604de6dee2e095c61355c6cefe3a00b98e50b83f
Binod <it21211164@my.sliit.lk>
2024-10-05 22:50:52 +0530

    Did some changes in main page

--- app/src/main/java/com/example/ead_ecommerce_app/Activity/BaseActivity.kt
+++ app/src/main/java/com/example/ead_ecommerce_app/Activity/BaseActivity.kt
@@ -1,21 +1,19 @@
 package com.example.ead_ecommerce_app.Activity
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.ead_ecommerce_app.R
 
-class BaseActivity : AppCompatActivity() {
+open class BaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_base)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+
     }
 }
--- app/src/main/java/com/example/ead_ecommerce_app/Activity/MainActivity.kt
+++ app/src/main/java/com/example/ead_ecommerce_app/Activity/MainActivity.kt
@@ -1,21 +1,70 @@
 package com.example.ead_ecommerce_app.Activity
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
+import com.example.ead_ecommerce_app.Adapter.CategoryAdapter
+import com.example.ead_ecommerce_app.Adapter.SliderAdapter
+import com.example.ead_ecommerce_app.Model.SliderModel
 import com.example.ead_ecommerce_app.R
+import com.example.ead_ecommerce_app.ViewModel.MainViewModel
+import com.example.ead_ecommerce_app.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel = MainViewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initBanner()
+        initCategory()
+    }
+
+    private fun initCategory(){
+        binding.progressBarCategories.visibility=View.VISIBLE
+        viewModel.categories.observe(this, Observer {
+            binding.recyclerViewCategories.layoutManager=LinearLayoutManager(this@MainActivity,LinearLayoutManager.HORIZONTAL,false)
+            binding.recyclerViewCategories.adapter=CategoryAdapter(it)
+            binding.progressBarCategories.visibility=View.GONE
+        })
+        viewModel.loadCategory()
+    }
+
+    private fun banners(image:List<SliderModel>){
+        binding.viewPager2.adapter = SliderAdapter(image,binding.viewPager2)
+        binding.viewPager2.clipToPadding = false
+        binding.viewPager2.clipChildren = false
+        binding.viewPager2.offscreenPageLimit = 3
+        binding.viewPager2.getChildAt(0).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
+
+        val compositePageTransformer = CompositePageTransformer().apply{
+            addTransformer(MarginPageTransformer(40))
         }
+        binding.viewPager2.setPageTransformer(compositePageTransformer)
+
+        if(image.size > 1){
+            binding.dotIndicator.visibility = View.VISIBLE
+            binding.dotIndicator.attachTo(binding.viewPager2)
+        }
+    }
+
+    private fun initBanner(){
+        binding.progressBarSlider.visibility = View.VISIBLE
+        viewModel.banners.observe(this, Observer {
+            banners(it)
+            binding.progressBarSlider.visibility = View.GONE
+        })
+        viewModel.loadBanners()
     }
 }
