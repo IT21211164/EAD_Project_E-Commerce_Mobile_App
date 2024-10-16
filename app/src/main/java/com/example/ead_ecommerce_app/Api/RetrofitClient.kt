6379ab7c76a5191e67bdcd9a2e0f2b64e2337aa7
IT21189494 <it21189494@my.sliit.lk>
2024-10-16 10:59:12 +0530

    ip changed

--- app/src/main/java/com/example/ead_ecommerce_app/Api/RetrofitClient.kt
+++ app/src/main/java/com/example/ead_ecommerce_app/Api/RetrofitClient.kt
@@ -3,7 +3,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://192.168.8.157:8082/api/" // base url
+    private const val BASE_URL = "http://192.168.93.139:8082/api/" // base url
 
     val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
